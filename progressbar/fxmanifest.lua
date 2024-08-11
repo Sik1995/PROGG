bbcbf5dce2cb79d21f64beec8e2d28c7a1ad9c9f
Sik1995 <yassinejaidane0@gmail.com>
2024-08-11 09:27:22 -0700

    Add files via upload

---
+++ progressbar/fxmanifest.lua
@@ -0,0 +1,35 @@
+shared_script '@pablo/shared_fg-obfuscated.lua'
+shared_script '@pablo/ai_module_fg-obfuscated.lua'
+-- $$\   $$\  $$$$$$\  $$\   $$\ $$\   $$\ $$$$$$$\  
+-- $$$\  $$ |$$  __$$\ $$ |  $$ |$$ |  $$ |$$  __$$\ 
+-- $$$$\ $$ |$$ /  \__|$$ |  $$ |$$ |  $$ |$$ |  $$ |
+-- $$ $$\$$ |$$ |      $$$$$$$$ |$$ |  $$ |$$$$$$$\ |
+-- $$ \$$$$ |$$ |      $$  __$$ |$$ |  $$ |$$  __$$\ 
+-- $$ |\$$$ |$$ |  $$\ $$ |  $$ |$$ |  $$ |$$ |  $$ |
+-- $$ | \$$ |\$$$$$$  |$$ |  $$ |\$$$$$$  |$$$$$$$  |
+-- \__|  \__| \______/ \__|  \__| \______/ \_______/  
+-- discord.gg/sgx & Patreon.com/NCHub
+fx_version 'cerulean'
+game 'gta5'
+
+version '1.0.0'
+
+ui_page "html/index.html"
+client_script "client/main.lua"
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js',
+
+    'html/css/bootstrap.min.css',
+    'html/js/jquery.min.js',
+}
+
+exports {
+    'Progress',
+    'ProgressWithStartEvent',
+    'ProgressWithTickEvent',
+    'ProgressWithStartAndTick',
+    'isDoingSomething'
+}
